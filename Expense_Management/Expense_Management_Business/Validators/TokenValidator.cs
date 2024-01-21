16a224ac316b9ba9e5e07e6833b226d9c27cc303
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-22 01:34:00 +0300

    TokenValidator added.

---
+++ Expense_Management/Expense_Management_Business/Validators/TokenValidator.cs
@@ -0,0 +1,13 @@
+﻿using Expense_Management_Schema.Token.Requests;
+using FluentValidation;
+
+namespace Expense_Management_Business.Validators;
+
+public class TokenValidator : AbstractValidator<TokenRequest>
+{
+    public TokenValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().MinimumLength(4).MaximumLength(50);
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(5).MaximumLength(50);
+    }
+}
