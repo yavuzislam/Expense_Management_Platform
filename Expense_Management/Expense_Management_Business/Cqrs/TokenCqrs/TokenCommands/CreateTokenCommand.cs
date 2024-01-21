205df9debb9fa4dbc4abe3e85fa22f16f360d879
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-22 01:30:28 +0300

    TokenCqrs added.

---
+++ Expense_Management/Expense_Management_Business/Cqrs/TokenCqrs/TokenCommands/CreateTokenCommand.cs
@@ -0,0 +1,8 @@
+﻿using Expense_Management_Base.Response;
+using Expense_Management_Schema.Token.Requests;
+using Expense_Management_Schema.Token.Responses;
+using MediatR;
+
+namespace Expense_Management_Business.Cqrs.TokenCqrs.TokenCommands;
+
+public record CreateTokenCommand(TokenRequest Model) : IRequest<ApiResponse<TokenResponse>>;
