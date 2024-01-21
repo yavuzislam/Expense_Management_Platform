1bebfe6bf471a45fcc4f1687e0758657efc10b99
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-22 01:44:14 +0300

    CategoryValidator added.

---
+++ Expense_Management/Expense_Management_Business/Validators/CategoryValidator.cs
@@ -0,0 +1,17 @@
+﻿using Expense_Management_Schema.Categories.Requests;
+using FluentValidation;
+
+namespace Expense_Management_Business.Validators;
+
+public class CategoryValidator : AbstractValidator<CategoryRequest>
+{
+    public CategoryValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Category name cannot be empty")
+            .Length(1, 50).WithMessage("Category name must be between 1 and 50 characters");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(100).WithMessage("Description must be less than or equal to 100 characters");
+    }
+}
