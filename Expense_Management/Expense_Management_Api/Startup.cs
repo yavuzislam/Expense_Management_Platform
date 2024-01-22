896815c6ac138e497e360fe1ce7f858622adde82
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-22 03:55:36 +0300

    Update Startup.cs

--- Expense_Management/Expense_Management_Api/Startup.cs
+++ Expense_Management/Expense_Management_Api/Startup.cs
@@ -1,4 +1,27 @@
-﻿namespace Expense_Management_Api;
+﻿using System.Data;
+using System.Reflection;
+using System.Text;
+using AutoMapper;
+using Expense_Management_Api.Middlewares;
+using Expense_Management_Base.Token;
+using Expense_Management_Business.Cqrs.UserCqrs.UserCommands;
+using Expense_Management_Business.Mapper;
+using Expense_Management_Business.Services.NotificationService;
+using Expense_Management_Business.Services.PaymentService;
+using Expense_Management_Business.Services.ReportService;
+using Expense_Management_Business.Validators;
+using Expense_Management_Data.Context;
+using FluentValidation.AspNetCore;
+using Hangfire;
+using Hangfire.SqlServer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
+
+namespace Expense_Management_Api;
 
 public class Startup
 {
@@ -11,9 +34,101 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers();
+        string connection = Configuration.GetConnectionString("MsSqlConnection");
+        services.AddDbContext<ExpenseManagementDbContext>(options => options.UseSqlServer(connection));
+
+        services.AddTransient<IDbConnection>(sp => new SqlConnection(connection));
+        
+
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateUserCommand).GetTypeInfo().Assembly));
+
+        var mapperConfig = new MapperConfiguration(cfg => cfg.AddProfile(new MapperConfig()));
+        services.AddSingleton(mapperConfig.CreateMapper());
+
+        services.AddControllers().AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<UserValidator>());
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        
+        services.AddResponseCaching();
+        services.AddMemoryCache();
+        
+        var redisConfig = new ConfigurationOptions();
+        redisConfig.EndPoints.Add(Configuration["Redis:Host"],Convert.ToInt32(Configuration["Redis:Port"]));
+        redisConfig.DefaultDatabase = 0;
+        services.AddStackExchangeRedisCache(opt =>
+        {
+            opt.ConfigurationOptions = redisConfig;
+            opt.InstanceName = Configuration["Redis:InstanceName"];
+        });
+
+        
+        
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Expense Management Api", Version = "v1.0" });
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "Expense Management for IT Company",
+                Description = "Enter JWT Bearer token **_only_**",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+            c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                { securityScheme, new string[] { } }
+            });
+        });
+        
+        var jwtConfig = Configuration.GetSection("JwtConfig").Get<JwtConfig>();
+        services.AddSingleton(jwtConfig);
+        
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = true;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtConfig.Issuer,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtConfig.Secret)),
+                ValidAudience = jwtConfig.Audience,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromMinutes(2)
+            };
+        });
+        
+        services.AddHangfire(configuration => configuration
+            .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+            .UseSimpleAssemblyNameTypeSerializer()
+            .UseRecommendedSerializerSettings()
+            .UseSqlServerStorage(Configuration.GetConnectionString("HangfireSqlConnection"), new SqlServerStorageOptions
+            {
+                CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                QueuePollInterval = TimeSpan.FromSeconds(15),
+                UseRecommendedIsolationLevel = true,
+                DisableGlobalLocks = true 
+            }));
+        services.AddHangfireServer();
+        
+        services.AddScoped<INotificationService, NotificationService>();
+        services.AddScoped<IPaymentService, PaymentService>();
+        services.AddScoped<IReportService, ReportService>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -25,7 +140,15 @@ public class Startup
             app.UseSwaggerUI();
         }
 
+        app.UseMiddleware<HeartBeatMiddleware>();
+        app.UseMiddleware<ErrorHandlerMiddleware>();
+
         app.UseHttpsRedirection();
+        
+        app.UseResponseCaching();
+        app.UseHangfireDashboard();
+        
+        app.UseAuthentication();
         app.UseRouting();
         app.UseAuthorization();
         app.UseEndpoints(x => { x.MapControllers(); });
