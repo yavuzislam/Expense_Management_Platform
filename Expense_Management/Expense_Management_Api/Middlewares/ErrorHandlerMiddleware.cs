57f5e9d12b2cf4c3de93d0f03ec0545a9dedadf2
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-22 03:49:50 +0300

    Middlewares added.

---
+++ Expense_Management/Expense_Management_Api/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Text.Json;
+using Serilog;
+
+namespace Expense_Management_Api.Middlewares;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        this.next = next;
+    }
+
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (Exception exception)
+        {
+            Log.Error(exception,"UnexpectedError");
+            Log.Fatal(                        
+                $"Path={context.Request.Path} || " +                      
+                $"Method={context.Request.Method} || " +
+                $"Exception={exception.Message}"
+            );
+            
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize("Internal error!"));
+        }
+    }
+}
---
+++ Expense_Management/Expense_Management_Api/Middlewares/HeartBeatMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Expense_Management_Api.Middlewares;
+
+public class HeartBeatMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public HeartBeatMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Path.StartsWithSegments("/hello"))
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            await context.Response.WriteAsync(JsonSerializer.Serialize("Hello from server"));
+            return;
+        }
+
+        await _next.Invoke(context);
+    }
+}
