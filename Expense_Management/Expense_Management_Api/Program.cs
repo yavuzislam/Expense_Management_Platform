a645684da61d94c2c4343d511b460479282cfddd
Yavuz Ozdemir <islam.ozdemir41@gmail.com>
2024-01-22 03:55:42 +0300

    Update Program.cs

--- Expense_Management/Expense_Management_Api/Program.cs
+++ Expense_Management/Expense_Management_Api/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Expense_Management_Api;
+﻿using Serilog;
+
+namespace Expense_Management_Api;
 
 public class Program
 {
     public static void Main(string[] args)
     {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+        Log.Information("App server is starting.");
         Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }).Build().Run();
     }
 }
